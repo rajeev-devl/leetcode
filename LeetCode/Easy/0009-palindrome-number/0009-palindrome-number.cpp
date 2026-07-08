6e27b5c8a873a8484343cc3dbe06b544d9c0b38c
rajeev-devl <rajeevyadav.142005@gmail.com>
2026-07-08 15:30:46 +0530

    Time: 3 ms (44.91%), Space: 8.6 MB (38.07%) - LeetHub

--- LeetCode/Easy/0009-palindrome-number/0009-palindrome-number.cpp
+++ LeetCode/Easy/0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,21 +1,38 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        int n = x;
-        int res = 0;
-        while(n>0){
-            int rev = n%10;
-            if(res>INT_MAX/10 || res<INT_MIN/10){
-                return 0;
-            }
-            res = res*10+rev;
-            n/=10;
-            
-        }
-        if(res==x){
-            return true;
-        }else{
+        // more optimal solution here the time complexity is O(logN/2)
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
             return false;
         }
+        int res = 0;
+        while (x > res) {
+            int rev = x % 10;
+            res = res * 10 + rev;
+            x /= 10;
+        }
+        //for odd we are diving with 10 as it ignores the middle element
+        return x==res||x==res/10;
+
+
+        /// lesss optimal approach  (original approach that i solved) here the
+        /// time complexity is O(logN) which is much slower than the optimal
+        /// solution
+        // int n = x;
+        // int res = 0;
+        // while(n>0){
+        //     int rev = n%10;
+        //     if(res>INT_MAX/10 || res<INT_MIN/10){
+        //         return 0;
+        //     }
+        //     res = res*10+rev;
+        //     n/=10;
+
+        // }
+        // if(res==x){
+        //     return true;
+        // }else{
+        //     return false;
+        // }
     }
 };
