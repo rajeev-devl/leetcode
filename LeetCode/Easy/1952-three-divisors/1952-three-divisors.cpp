c0aa404972266f9d1837487fd0d5082fa03a9ba5
rajeev-devl <rajeevyadav.142005@gmail.com>
2026-07-08 19:00:43 +0530

    Time: 0 ms (100%), Space: 7.8 MB (59.51%) - LeetHub

--- LeetCode/Easy/1952-three-divisors/1952-three-divisors.cpp
+++ LeetCode/Easy/1952-three-divisors/1952-three-divisors.cpp
@@ -1,19 +1,36 @@
 class Solution {
 public:
     bool isThree(int n) {
-        //int k = 0;
+        //optimal solution as the time complexity here is O(root(n))
+        int root = sqrt(n);
+        if(root*root !=n){
+            return false;
+        }
+        if(n<2){
+            return false;
+        }
 
-        int x = n;
-        int count = 0;
-        while(x>0){
-            if(n%(x)==0){
-                count = count +1;
+
+        for(int i=2;i*i<=root;i++){
+            if(root%i==0){
+                return false;
             }
-            x--;
         }
-        if(count==3){
-            return true;
-        }
-        return false;
+        return true;
+
+
+        /// this wrong timee complexity is O(n)
+        // int x = n;
+        // int count = 0;
+        // while(x>0){
+        //     if(n%(x)==0){
+        //         count = count +1;
+        //     }
+        //     x--;
+        // }
+        // if(count==3){
+        //     return true;
+        // }
+        // return false;
     }
 };
