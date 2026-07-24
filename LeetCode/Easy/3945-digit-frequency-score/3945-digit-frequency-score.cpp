e64c57a45b6d2e111d3700b9be94067fc9261c0b
rajeev-devl <rajeevyadav.142005@gmail.com>
2026-07-24 09:23:00 +0530

    Time: 0 ms (100%), Space: 8.6 MB (80.84%) - LeetHub

--- LeetCode/Easy/3945-digit-frequency-score/3945-digit-frequency-score.cpp
+++ LeetCode/Easy/3945-digit-frequency-score/3945-digit-frequency-score.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
     int digitFrequencyScore(int n) {
+        int freq[10] = {0};
         int res = 0;
         while(n>0){
             int rem = n%10;
-            res += rem;
+            freq[rem]++;
             n/=10;
+        }
+        for(int i = 0;i<10;i++){
+            res += i*freq[i];
         }
         return res;
     }
