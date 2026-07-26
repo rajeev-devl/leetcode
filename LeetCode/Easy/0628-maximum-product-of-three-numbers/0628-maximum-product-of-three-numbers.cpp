1e444dd52ec1f17e5fe58614fff9f98a5e2bc731
rajeev-devl <rajeevyadav.142005@gmail.com>
2026-07-26 11:03:40 +0530

    Time: 3 ms (65.12%), Space: 31.5 MB (46.18%) - LeetHub

--- LeetCode/Easy/0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ LeetCode/Easy/0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -1,32 +1,42 @@
 class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
-        // int n = nums.size();
-        // int first = 0;
-        // int second = 0;
-        // int third = 0;
-        // for(int i=0;i<n;i++){
-        //     if(arr[i]>first){
-        //         third = second;
-        //         second = first;
-        //         first = arr[i];
-        //     }else if(arr[i]>second){
-        //         third = second;
-        //         second = arr[i];
-        //     }else{
-        //         third = arr[i];
-        //     }
-        // }
-        // return first*second*third;
         int n = nums.size();
-        sort(nums.begin(),nums.end());
-        int first = nums[n-1]*nums[n-2]*nums[n-3];
-        int second = nums[0]*nums[1]*nums[n-1];
-        
-        return max(first,second);
+        int first = INT_MIN;
+        int second = INT_MIN;
+        int third = INT_MIN;
 
-        // for(int i=0;i<n;i++){
-        //     first *= arr[i]*arr[i-1]
-        // }
+        int first_min = INT_MAX;
+        int second_min = INT_MAX;
+
+        for(int i=0;i<n;i++){
+            if(nums[i]>first){
+                third = second;
+                second = first;
+                first = nums[i];
+            }else if(nums[i]>second){
+                third = second;
+                second = nums[i];
+            }else if(nums[i]>third){
+                third = nums[i];
+            }
+
+            if(nums[i]<=first_min){
+                second_min = first_min;
+                first_min=nums[i];
+            }
+            else if(nums[i]<=second_min){
+                second_min = nums[i];
+            }
+        }
+        int res1 = first*second*third;
+        int res2 = first_min*second_min*first;
+        return max(res1,res2);
+        // int n = nums.size();
+        // sort(nums.begin(),nums.end());
+        // int first = nums[n-1]*nums[n-2]*nums[n-3];
+        // int second = nums[0]*nums[1]*nums[n-1];
+        
+        // return max(first,second);
     }
 };
