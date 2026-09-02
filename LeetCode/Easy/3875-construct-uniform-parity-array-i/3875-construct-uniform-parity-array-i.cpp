29051d400c9d3ac17cfa612c0781854ff00dfe6e
rajeev-devl <rajeevyadav.142005@gmail.com>
2026-09-02 18:03:04 +0530

    Time: 3 ms (5.87%), Space: 30.2 MB (67.8%) - LeetHub

--- LeetCode/Easy/3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
+++ LeetCode/Easy/3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -1,6 +1,53 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        return true;
+        // int n=nums1.size();
+        // vector<int> nums2(n);
+        // bool l=false;
+        // for(int i=0;i<n-1;++i){
+        //     nums2[i]=nums1[i]-nums1[i+1];
+        // }
+        // for(int i=0;i<n-1;++i){
+        //     if(nums2[i]%2==0){
+        //         if(nums2[i+1]%2==0){
+        //             l=true;
+        //         }
+        //         else{
+        //             l=false;
+        //         }
+        //     }
+        //     else{
+        //         if(nums2[i+1]%2!=0){
+        //             l=true;
+        //         }
+        //         else{
+        //             l=false;
+        //         }
+        //     }
+        // }
+        int n=nums1.size();
+        bool l=false;
+        bool t=false;
+        for(int i=0;i<n;++i){
+            if(nums1[i]%2!=0){
+                l=true;
+            }
+            else{
+                t=true;
+            }
+        }
+        if(t && l){
+            return true;
+        }
+        if(!t && l){
+            return true;
+        }
+        if(t && !l){
+            return true;
+        }
+        if(!t && !l){
+            return false;
+        }
+        return false;
     }
 };
