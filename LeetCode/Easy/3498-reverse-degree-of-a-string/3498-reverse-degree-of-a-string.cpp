13450d3378106672c9be2786a3b0f14bc55526bf
rajeev-devl <rajeevyadav.142005@gmail.com>
2026-09-20 22:37:48 +0530

    Time: 0 ms (100%), Space: 9.7 MB (47.88%) - LeetHub

---
+++ LeetCode/Easy/3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int sum = 0;
+        for(int i = 0;i<s.size();i++){
+            int rev = 26-(s[i]-'a');
+            int pos = i+1;
+            sum += rev*pos; 
+        }
+        return sum;
+    }
+};
