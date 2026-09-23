17e767df2a151cce35249bcfb4b3ad71dad11741
rajeev-devl <rajeevyadav.142005@gmail.com>
2026-09-23 23:38:45 +0530

    Time: 0 ms (100%), Space: 15.2 MB (15.96%) - LeetHub

--- LeetCode/Easy/2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
+++ LeetCode/Easy/2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
@@ -2,11 +2,25 @@ class Solution {
 public:
     vector<int> targetIndices(vector<int>& nums, int target) {
         vector<int>res;
-        sort(nums.begin(),nums.end());
+        // sort(nums.begin(),nums.end());
+        // for(int i = 0;i<nums.size();i++){
+        //     if(nums[i]==target){
+        //         res.push_back(i);
+        //     }
+        // }
+        int a = 0;
+        int freq = 0;
         for(int i = 0;i<nums.size();i++){
-            if(nums[i]==target){
-                res.push_back(i);
+            if(nums[i]<target){
+                a++;
             }
+            if(nums[i]==target){
+                freq++;
+            }
+        }
+
+        for(int i = 0;i<freq;i++){
+            res.push_back(a+i);
         }
         return res;
     }
