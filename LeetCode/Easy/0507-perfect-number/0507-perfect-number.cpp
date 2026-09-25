0d4aa6f96fdbb9bf4e5ba2a584fd7a309b875d49
rajeev-devl <rajeevyadav.142005@gmail.com>
2026-09-25 17:07:56 +0530

    Time: 0 ms (100%), Space: 7.8 MB (21.43%) - LeetHub

--- LeetCode/Easy/0507-perfect-number/0507-perfect-number.cpp
+++ LeetCode/Easy/0507-perfect-number/0507-perfect-number.cpp
@@ -1,10 +1,19 @@
 class Solution {
 public:
     bool checkPerfectNumber(int num) {
-        int sum = 0;
-        for(int i = 1;i<num;i++){
+        int sum = 1;
+        // for(int i = 1;i<num;i++){
+        //     if(num%i==0){
+        //         sum += i;
+        //     }
+        // }
+        if(num==1) return false;
+        for(int i = 2;i<=sqrt(num);i++){
             if(num%i==0){
                 sum += i;
+                if(num/i!=i){
+                    sum += num/i;
+                }
             }
         }
         if(sum==num){
