b498b3f49081157df9f6f7daadb318bcafed4cfe
rajeev-devl <rajeevyadav.142005@gmail.com>
2026-09-25 09:28:33 +0530

    Time: 0 ms (100%), Space: 22 MB (12.1%) - LeetHub

--- LeetCode/Easy/0268-missing-number/0268-missing-number.cpp
+++ LeetCode/Easy/0268-missing-number/0268-missing-number.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        vector<int>freq(nums.size()+1,0);
-        for(int x:nums){
-            freq[x]++;
+        // vector<int>freq(nums.size()+1,0);
+        // for(int x:nums){
+        //     freq[x]++;
+        // }
+        // for(int i = 0;i<=nums.size();i++){
+        //     if(freq[i]==0){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+        // tc-O(n) sc-O(n)
+        int sum = 0;
+        int n = nums.size();
+        int actual = (n*(n+1))/2;
+        for(int i = 0;i<n;i++){
+            sum += nums[i];
         }
-        for(int i = 0;i<=nums.size();i++){
-            if(freq[i]==0){
-                return i;
-            }
-        }
-        return -1;
+        return actual - sum;
     }
 };
